7ef417fbe8a68e35fb30b24530c2345f4724e9ed
Josue-lab <58716020+Josue-lab@users.noreply.github.com>
2020-02-04 15:38:29 -0800

    Update Main.scala

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,9 @@ object Main extends App {
     Person(firstName = "Peter", lastName = "Müller", age = 34),
     Person(firstName = "Nick", lastName = "Tagart", age = 52))
   val adults = Person.filterAdult(persons)
-  val kids = Person.filterkids(persons)
+  val kids = Person.filterKids(persons)
   val descriptions = adults.map(p => p.description).mkString("\n\t")
-  val descriptions1 = kids.map(p => p.description).mkString("\n\t")
+  val descriptions1 = kids.map(p <= p.description).mkString("\n\t")
   println(s"The adults are \n\t$descriptions")
   println(s"The kids is \n\t$descriptions1")
 }
